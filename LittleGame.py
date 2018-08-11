85064a229701f95149e285780bf66969d57af905
bigfoolliu <liu15671677014@icloud.com>
2018-08-11 22:10:30 +0800

    first commit,修复了众多bug,现已正常运行.

--- LittleGame.py
+++ LittleGame.py
@@ -3,6 +3,11 @@
 # !@Author: Liu Rui
 # !@github: bigfoolliu
 
+
+"""
+三子连珠游戏
+"""
+
 import pygame
 from pygame.locals import *
 from sys import exit
@@ -15,13 +20,11 @@ chess_num = 0  # 棋盘上的棋子数量,默认为0,最多为9
 score_o = 0
 score_x = 0
 
+game_status = 0  # 游戏状态,0表示未分出胜负,1表示胜负已经产生
+
 
 def draw_rect(screen):
-	"""
-	画棋盘
-	:param screen:
-	:return:
-	"""
+	"""画棋盘"""
 	for i in range(0, 9):
 		if 0 <= i < 3:
 			pygame.draw.rect(screen, (0, 0, 0), (150 + 100 * i, 150, 100, 100), 10)
@@ -32,11 +35,7 @@ def draw_rect(screen):
 
 
 def start_font_display(screen):
-	"""
-	显示游戏标题,游戏信息,版权等
-	:param screen:
-	:return:
-	"""
+	"""显示游戏标题,游戏信息,版权等"""
 	# 游戏标题显示
 	game_caption = "Little Game"
 	game_caption_font = pygame.font.SysFont("arial black", 40, bold=True)
@@ -83,13 +82,7 @@ def start_font_display(screen):
 
 
 def score_display(screen, _score_o, _score_x):
-	"""
-	显示游戏的得分
-	:param screen:
-	:param _score_o: int
-	:param _score_x: int
-	:return:
-	"""
+	"""显示游戏的得分"""
 	score_o_info = str(_score_o)
 	score_x_info = str(_score_x)
 	score_font = pygame.font.SysFont("arial black", 50)
@@ -97,16 +90,10 @@ def score_display(screen, _score_o, _score_x):
 	score_x_info_surface = score_font.render(score_x_info, True, (0, 0, 0), (255, 255, 255))
 	screen.blit(score_o_info_surface, (45, 395))
 	screen.blit(score_x_info_surface, (510, 395))
-	pygame.display.update()
 
 
 def game_over_font_display(screen, winner):
-	"""
-	游戏结束时显示文字
-	:param screen:
-	:param winner: str, game胜者
-	:return:
-	"""
+	"""游戏结束时显示文字"""
 	# Game Over显示
 	game_over_font = pygame.font.SysFont("arial black", 40, bold=True)
 	game_over_surface = game_over_font.render("Game Over", True, (0, 0, 0), (255, 255, 255))
@@ -122,19 +109,9 @@ def game_over_font_display(screen, winner):
 	winner_rect = Rect((210, 470), (200, 10))
 	screen.blit(winner_surface, winner_rect)
 
-	pygame.display.update()
-
 
 def draw_circle(screen, position_x, position_y, radis, width=0):
-	"""
-	画圆
-	:param screen:
-	:param position_x: 所画圆的中心位置横坐标
-	:param position_y: 所画圆的中心位置的纵坐标
-	:param radis: 圆的直径
-	:param width: 圆的线宽
-	:return:
-	"""
+	"""画圆"""
 	circle = pygame.draw.circle(screen, (0, 0, 0), (position_x, position_y), radis, width)
 	return circle
 
@@ -162,7 +139,7 @@ def create_grid():
 		for col in range(0, 3):  # 遍历列
 			grid = {}
 			grid["num"] = (row, col)
-			grid["coordinate"] = ((150 + 100 * col), (150 + 100 * row))
+			grid["coordinate"] = ((150 + 100 * col), (150 + 100 * row))  # 每个棋格的坐标
 			grid["occupy_flag"] = 0  # 0表示默认未被占用
 			grid["chess"] = None  # 记录每个棋盘放置的是什么棋子0表示o, 1表示叉
 			list1.append(grid)
@@ -185,8 +162,6 @@ def traffic_light(screen):
 		pygame.draw.circle(screen, (255, 0, 0), (65, 290), 25, 25)
 		pygame.draw.circle(screen, (84, 155, 120), (525, 230), 25, 25)
 		pygame.draw.circle(screen, (0, 0, 0), (525, 290), 25, 25)
-
-	pygame.display.update()
 
 
 def put_chess(screen, grid, position_x, position_y):
@@ -216,16 +191,19 @@ def judge_game(_grid_list):
 	:param _grid_list: 全局棋盘信息
 	:return:
 	"""
+	global game_status
 	# 判断横着的是否有胜利的
 	for row in range(0, 3):
 		if (_grid_list[row][0]["chess"] == _grid_list[row][1]["chess"] == _grid_list[row][2]["chess"] == 0) or \
 				(_grid_list[row][0]["chess"] == _grid_list[row][1]["chess"] == _grid_list[row][2]["chess"] == 1):
+			game_status = 1  # 更新游戏状态
 			return True
 
 	# 判断竖着的是否有胜利的
 	for col in range(0, 3):
 		if (_grid_list[0][col]["chess"] == _grid_list[1][col]["chess"] == _grid_list[2][col]["chess"] == 0) or \
 				(_grid_list[0][col]["chess"] == _grid_list[1][col]["chess"] == _grid_list[2][col]["chess"] == 1):
+			game_status = 1  # 更新游戏状态
 			return True
 
 	# 判断斜的是否有胜利的
@@ -233,85 +211,101 @@ def judge_game(_grid_list):
 			(_grid_list[0][2]["chess"] == _grid_list[1][1]["chess"] == _grid_list[2][0]["chess"] == 0) or \
 			(_grid_list[0][0]["chess"] == _grid_list[1][1]["chess"] == _grid_list[2][2]["chess"] == 1) or \
 			(_grid_list[0][2]["chess"] == _grid_list[1][1]["chess"] == _grid_list[2][0]["chess"] == 1):
+		game_status = 1  # 更新游戏状态
 		return True
+	return False
 
 
-def judge_winner(_score_x, _score_o):
+def judge_winner():
 	"""
 	游戏结束时棋盘上的棋子
 	:return:
 	"""
-	if chess_num >= 6 and chess_num % 2 == 0:  # X赢
-		_score_x += 1
+	global score_x, score_o
+	if 9 >= chess_num >= 6 and chess_num % 2 == 0:  # X赢
+		score_x += 1
 		return "X"
-	elif chess_num >= 5 and chess_num % 2 == 1:  # O赢
-		_score_o += 1
+	elif 9 >= chess_num >= 5 and chess_num % 2 == 1:  # O赢
+		score_o += 1
 		return "O"
 
 
-def game_reset(mouse_pos, screen):
+def game_start(screen):
 	"""
-	按下reset,将游戏的比分重置
-	:param screen:
-	:param mouse_pos:
+	游戏重新开始以及开始显示画面
 	:return:
 	"""
-	global score_o, score_x
-	if 4 <= mouse_pos[0] <= 104 and 555 <= mouse_pos[1] <= 595:
-		print("reset")
-		score_o = 0
-		score_x = 0
-		score_display(screen, score_o, score_x)
-
-
-def game_restart(mouse_pos):
-	"""
-
-	:param mouse_pos:
-	:return:
-	"""
-	if 495 <= mouse_pos[0] <= 595 and 555 <= mouse_pos[1] <= 595:
-		print("restart")
-
-
-def main():
-	pygame.init()
-	screen = pygame.display.set_mode((600, 600))
-
 	screen.fill((255, 255, 255))  # 窗口背景填充为白色
-
 	draw_rect(screen)  # 画棋盘
 	start_font_display(screen)  # 显示初始信息
 
 	score_display(screen, score_o, score_x)  # 屏幕显示比分
+	traffic_light(screen)  # 显示下棋顺序
 
-	pygame.display.update()  # 这里更新,已显示背景,即不变得部分
 
-	create_grid()  # 创建每个棋格的信息
+def game_reset(screen):
+	"""按下reset,将游戏的比分重置"""
+	print("reset")
+	global score_o, score_x
+	score_o = 0
+	score_x = 0
+	game_restart(screen)
+
+
+def game_restart(screen):
+	"""
+	游戏重新开始以及开始显示画面
+	:return:
+	"""
+	global game_status
+	print("restart")
+	for row in range(0, 3):
+		for col in range(0, 3):
+			grid_list[row][col]["occupy_flag"] = 0
+			grid_list[row][col]["chess"] = None
+	game_status = 0
+	game_start(screen)
+
+
+def main():
+	global chess_num
+	global game_status
+	pygame.init()
+
+	screen = pygame.display.set_mode((600, 600))
+	game_start(screen)  # 开始显示
+	create_grid()  # 棋格信息初始化
+	winner = "None"
 
 	# 需要动态更新的都要放置在游戏主循环
 	while True:
-
-		traffic_light(screen)  # 显示下棋顺序
-		score_display(screen, score_o, score_x)  # 显示游戏比分
-
-		mouse_pos = pygame.mouse.get_pos()
+		mouse_pos = pygame.mouse.get_pos()  # 获取鼠标的位置
 
 		event = pygame.event.wait()
 		# 发生退出事件
 		if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
 			exit()
 
-		# 按下鼠标按键
-		if event.type == MOUSEBUTTONDOWN:
-			pressed_array = pygame.mouse.get_pressed()
-			if pressed_array[0] == 1:  # 鼠标左键按下
+		# 按下鼠标左键
+		if event.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == 1:
+			# 如果鼠标在reset区域按下,游戏比分重置为0,棋盘重置
+			if 4 <= mouse_pos[0] <= 104 and 555 <= mouse_pos[1] <= 595:
+				game_reset(screen)
+			# 如果鼠标在restart区域按下,游戏比分不变,棋盘重置
+			elif 495 <= mouse_pos[0] <= 595 and 555 <= mouse_pos[1] <= 595:
+				game_restart(screen)
 
-				# 判断鼠标是否按下reset以及restart的反应
-				game_reset(mouse_pos, screen)
-				game_restart(mouse_pos)
+		# 判断游戏状态
+		if game_status == 1:  # 游戏胜负已经产生
+			score_display(screen, score_o, score_x)  # 显示游戏比分
+			game_over_font_display(screen, winner)  # 显示游戏结束信息
+			chess_num = 0
 
-				# 如果鼠标在棋盘里按下按下去
+		else:  # 游戏胜负仍然未产生
+			traffic_light(screen)  # 显示下棋顺序
+			score_display(screen, score_o, score_x)  # 显示游戏比分
+			if event.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == 1:
+				# 如果鼠标在棋盘里按下按下去,遍历检查鼠标的位置
 				for row in range(0, 3):
 					for col in range(0, 3):
 
@@ -324,27 +318,17 @@ def main():
 								put_chess(screen, grid_list[row][col], put_x, put_y)  # 放置棋子
 								grid_list[row][col]["occupy_flag"] = 1  # 放置棋子之后应更改该格的放置标志
 
-								# out_put_test2(grid)
-
-								game_result = judge_game(grid_list)
-								game_winner = judge_winner(score_x, score_o)
-								print(game_result)
-								if game_result is True:  # 游戏胜者产生,游戏结束
-									game_over_font_display(screen, game_winner)  # 显示胜者信息
-
-				if 4 <= mouse_pos[0] <= 104 and 555 <= mouse_pos[1] <= 595:
-					game_reset(mouse_pos, screen)
-				elif 495 <= mouse_pos[0] <= 595 and 555 <= mouse_pos[1] <= 595:
-					# game_restart(mouse_pos)
-					pass
-				else:
-					pygame.event.set_blocked(MOUSEBUTTONDOWN)  # 此时不再响应鼠标的按键按下
-
-			pygame.event.set_allowed(MOUSEBUTTONDOWN)
+								# 判断游戏结果以及是否有赢家
+								if judge_game(grid_list) and chess_num <= 9:
+									print(judge_game(grid_list))
+									winner = judge_winner()
+								elif (not judge_game(grid_list)) and chess_num == 9:
+									winner = "None"
+									game_status = 1
 
 		pygame.display.update()
 
-		time.sleep(0.02)  # 降低cpu的消耗
+		time.sleep(0.05)  # 降低cpu的消耗
 
 
 if __name__ == "__main__":
